7e4706173daef39d159739234bb9ca0be0bea961
Johnny0927 <113489273+Johnny0927@users.noreply.github.com>
2022-10-05 23:15:20 +0800

    Update Class01.java

--- Class01.java
+++ Class01.java
@@ -1,48 +1,40 @@
-class Cal_area
- {
- int radius, round;
- final double PI = 3.14159;
- int l=4, w=5, h=6;
- String name;
+package Java111Q1;
+import Java111Q1.Cal_area;
+class Cal_area {
+    int length;
+    int width;
+    int height;
+    int radius;
 
- void setR(int r)
- {
- radius=r;
- }
- void setP(int p) 
- {
- round=p;
- }
- void setName(String s)
- {
- name=s;
- }
- void show()
- {
- System.out.println("半徑="+ radius);
- System.out.println("球形體積="+ PI*(radius*radius));
- System.out.println("長="+l+",寬="+w+",高="+h);
- System.out.println("立方體表面積="+ (l*w)*h);
- }
- void setWindows(int r,int p)
- {
- radius=r;
- round=p;
- }
- void setWindows(int r,int p,String s)
- {
- radius=r;
- round=p;
- name=s;
- }
- }
-public class main {
-	public static void main(String[] args) {
-		Cal_area cw = new Cal_area();
-		 cw.setName("1st Windows");
-		 cw.setR(10);
-		 cw.setP(10);
-		 cw.show();
-	}
-
+    void showData1() {
+        System.out.println("長 = " + length);
+        System.out.println("寬 = " + width);
+        System.out.println("高 = " + height);
+    }
+    void showData2() {
+        System.out.println("半徑 =" + radius);
+    }
+    void surfaceArea() {
+        System.out.println("立方體表面積 = " + ((length * width + length * height + height * width) * 2));
+    }
+    void volume() {
+        System.out.printf("球形體積 = " + "%.1f", ((double) 3.14159 * radius * radius * radius * 4 / 3));
+        System.out.println();
+    }
+    void showAll() {
+        showData2();
+        volume();
+        showData1();
+        surfaceArea();
+    }
+}
+public class Class01 {
+    public static void main(String args[]) {
+        Cal_area area = new Cal_area();
+        area.length = 4;
+        area.width = 5;
+        area.height = 6;
+        area.radius = 10;
+        area.showAll();
+    }
 }
